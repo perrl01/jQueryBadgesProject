fa855db11e30b1fc90027ff816915708c9aba2a1
perrl01 <perrl01@yahoo.ca>
2017-10-30 02:01:37 -0400

    first commit

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,20 @@
 $(function() {
 
   // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/perrl01.json',
+    dataType: 'jsonp',
+    success: (response) => {
+      let $badges = $('#badges');
+      response.courses.completed.forEach((course) => {
+        let test = "123";
+        $badges.append(
+          `<div class="course">
+          <h3>${course.title}</h3>
+          <img src="${course.badge}"/>
+          <a href="${course.url}" target="_blank" class="btn btn-primary">See Course<a>
+          </div>`);
+    })}
+  })
 
 });
